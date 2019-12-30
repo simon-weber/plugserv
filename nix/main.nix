217882d3011f7ce424d0ef418eee16bd6731f951
Simon Weber <simon@simonmweber.com>
2019-12-29 22:01:53 -0500

    2x daily backups

--- nix/main.nix
+++ nix/main.nix
@@ -75,6 +75,7 @@ in let
 
     services.duplicity = {
       enable = true;
+      frequency = "*-*-* 00,12:00:00";
       root = "/tmp/db.backup";
       targetUrl = "pydrive://duply-alpha@repominder.iam.gserviceaccount.com/plugserv_backups/db3";
       secretFile = pkgs.writeText "dupl.env" ''

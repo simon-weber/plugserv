ad6881b34186f1896c5748b4d39dbdad68378386
Simon Weber <simon@simonmweber.com>
2021-04-15 12:27:27 -0400

    use sqlite backup api

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,8 +7,10 @@ print(settings.DATABASES['default']['NAME'])" \
   | python manage.py shell)
 db_name=$(basename "${db_path}")
 s3_path="plugserv/$(date +'%Y-%m-%d_%X')_${db_name}"
+backup_path="/tmp/${db_name}.bak"
 
+sqlite3 "${db_path}" ".backup '${backup_path}'"
 s3cmd --bucket-location=fr-par --host=https://s3.fr-par.scw.cloud --host-bucket=https://s3.fr-par.scw.cloud \
-  put "${db_path}" "s3://simoncodes-sqlite/${s3_path}"
+  put "${backup_path}" "s3://simoncodes-sqlite/${s3_path}"
 
 wget https://hc-ping.com/${HC_ID_BACKUP} -nv -T 10 -t 5 -O /dev/null
